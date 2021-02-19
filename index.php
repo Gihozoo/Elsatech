484f9907c3e3afe774442ee285b32f60bc3840e9
Gihozoo <70940050+Gihozoo@users.noreply.github.com>
2021-02-19 16:18:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once("index.html");
-include_once("information.html"); 
-include_once("c.html"); 
-include_once("more.html")?>
+<?php include_once("index.html")?>
+<?php include_once("information.html")?>
+<?php include_once("c.html")?>
+<?php include_once("more.html")?>
